ecebd1b07ad040b8c733eabc045e043887699e1f
Christian Peraza <christianperaza2000@gmail.com>
2025-01-15 10:37:33 -0300

    Create API documentation

--- controllers/temple.js
+++ controllers/temple.js
@@ -5,6 +5,8 @@ const apiKey =
   'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N';
 
 exports.create = (req, res) => {
+  /* #swagger.description = 'API Key if needed: Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N'
+   */
   // Validate request
   if (!req.body.name) {
     res.status(400).send({ message: 'Content can not be empty!' });
@@ -33,6 +35,8 @@ exports.create = (req, res) => {
 };
 
 exports.findAll = (req, res) => {
+  /* #swagger.description = 'API Key if needed: Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N'
+   */
   console.log(req.header('apiKey'));
   if (req.header('apiKey') === apiKey) {
     Temple.find(
@@ -62,6 +66,8 @@ exports.findAll = (req, res) => {
 
 // Find a single Temple with an id
 exports.findOne = (req, res) => {
+  /* #swagger.description = 'API Key if needed: Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N'
+   */
   const temple_id = req.params.temple_id;
   if (req.header('apiKey') === apiKey) {
     Temple.find({ temple_id: temple_id })
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const temple = require('./temple');
 
+routes.use('/', require('./swaggerRoute'));
 routes.use('/temples', temple);
 routes.use(
   '/',
